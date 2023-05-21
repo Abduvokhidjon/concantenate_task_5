2a342d6c34869c4ba20e79867d4ec19b41fc799a
Abduvokhidjon <aabduvohid33@gmail.com>
2023-05-21 11:47:04 -0500

    Task 5

---
+++ concanteante.js
@@ -0,0 +1,24 @@
+/*
+Task 5: Concatenate numbers and strings using the + operator
+
+
+Code three variables:
+
+The first variable should be a string with the following value: "I know ". Name the variable me.
+
+The second variable should be a number with the value: 3. Name the variable three.
+
+The third variable should a string with the following value: "programming languages". Name the variable proLang.
+
+Console log the following code: me + three + proLang.
+
+*/
+
+var me
+var three
+var proLang
+
+me = "I know "
+three = 3
+proLang = " programming languages"
+console.log(me + three + proLang)
